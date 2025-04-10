426edd648ae69b00e238aeac5e1570cc52fc6b22
sylviavitoria <sylviavitoriasilva@gmail.com>
2025-04-10 11:41:32 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/com/sylviavitoria/crud_pessoas/CrudPessoasApplication.java
@@ -0,0 +1,13 @@
+package com.sylviavitoria.crud_pessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sylviavitoria/crud_pessoas/CrudPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sylviavitoria.crud_pessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
